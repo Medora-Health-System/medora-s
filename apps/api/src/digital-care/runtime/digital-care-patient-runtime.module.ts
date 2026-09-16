a11f66d87467297067aa058a30c3c4cb56295c07
MasterMatz <audain@vyrth.com>
2026-09-16 17:10:50 -0500

    Wire facility configuration into patient Digital Care runtime

--- apps/api/src/digital-care/runtime/digital-care-patient-runtime.module.ts
+++ apps/api/src/digital-care/runtime/digital-care-patient-runtime.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { FacilityConfigurationModule } from "../../facility-configuration/facility-configuration.module";
 import { PatientPortalModule } from "../../patient-portal/public";
 import { DigitalCareMessagesController } from "./patient-self/digital-care-messages.controller";
 import { DigitalCarePortalStatusController } from "./patient-self/digital-care-portal-status.controller";
 
 @Module({
-  imports: [PatientPortalModule],
+  imports: [PatientPortalModule, FacilityConfigurationModule],
   controllers: [DigitalCarePortalStatusController, DigitalCareMessagesController],
 })
 export class DigitalCarePatientRuntimeModule {}
